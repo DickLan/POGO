88f5b402e14a60ce4f349fcb0cb1435ffcbc0443
DickLan <pokemaster556610@gmail.com>
2023-10-01 01:07:09 +0800

    get legend array

--- seeders/20230928071021-pogoAcct-seed-file.js
+++ seeders/20230928071021-pogoAcct-seed-file.js
@@ -10,7 +10,7 @@ module.exports = {
         team: 'red',
         price: 55555,
         contents_iv100: `噴火龍／火伊布／飛腿郎／大蔥鴨／打擊鬼／尼偶小人／單首龍／電擊獸／幼基拉斯／飛天螳螂／黑魯加／電龍／投摔鬼／頭蓋龍／三合一磁怪／鯉魚王／迷你龍／`,
-        contents_legend: '傳說佬／三三三',
+        contents_legend: '超夢／烈空座／波克雞絲／火焰鳥／拉帝歐斯／騎拉帝納／固拉多／藏馬然特／洛奇雅／傑克羅姆',
         account_id: 'B01',
         description: '留空',
         created_at: new Date(),
