3e27a2cd3628f1a7432a4da9c2ae2a06097c5055
DickLan <pokemaster556610@gmail.com>
2023-10-02 05:51:44 +0800

    add cart page

---
+++ controllers/user-controller.js
@@ -0,0 +1,8 @@
+
+
+const userController = {
+  getCart: (req, res, next) => {
+    res.render('users/cart')
+  }
+}
+module.exports = userController
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const accountController = require('../../controllers/account-controller')
+const userController = require('../../controllers/user-controller')
 
+// ========= account ==========
 // 測試用 之後要刪
 router.use('/accounts/detailTemp', accountController.getDetail)
 router.use('/accounts/:id', accountController.getAccount)
 router.use('/accounts', accountController.getAccounts)
+
+// ========= user ==========
+router.use('/cart', userController.getCart)
 
 // router.get('/accounts', (req, res) => {
 //   res.render('admin/accounts')
