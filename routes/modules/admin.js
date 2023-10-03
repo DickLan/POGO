ee00d3b31ffa17793d31c97cde5ed0ec9f432abc
DickLan <pokemaster556610@gmail.com>
2023-10-04 00:13:30 +0800

    add user admin path and pages, going to change admin accounts to excel or xml form

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const adminController = require('../../controllers/admin-controller')
 
-router.get('/accounts', (req, res, next) => {
-  res.render('accounts')
-})
+router.get('/accounts', adminController.getAccounts)
 
 module.exports = router
