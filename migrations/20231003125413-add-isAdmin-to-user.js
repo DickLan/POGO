fd109244aef6ac4faa3358a3043867e200ec6ba6
DickLan <pokemaster556610@gmail.com>
2023-10-03 21:43:28 +0800

    add user admin auth check

---
+++ migrations/20231003125413-add-isAdmin-to-user.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const { sequelize } = require('../models');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('Users', 'is_admin', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+
+    })
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Users', 'is_admin')
+  }
+};
