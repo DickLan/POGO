2f5b58babf027f647e3401c3ffde3a618d53ea4b
DickLan <pokemaster556610@gmail.com>
2023-10-05 15:11:59 +0800

    try to edit on same admin/accounts page, but got some issue now

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -8,9 +8,28 @@ const adminController = {
       // nest: true
     })
       .then(accounts => {
+        console.log(accounts)
 
         return res.render('admin/accounts', { accounts })
       })
+      .catch(err => next(err))
+  },
+  putAccount: (req, res, next) => {
+    // account在db的id (PK)
+    const dbActId = req.params.id
+    // load from modal form, and modal form load from previous getAccounts
+    const { accountId, level, startdust, price, contentsIv100, contentsLegend } = req.body
+    Account.findByPk(dbActId)
+      .then(acct => {
+        if (!acct) throw new Error('no this acct admin')
+        console.log('===================')
+        // console.log(acct)
+        return Account.update({
+
+          accountId, level, startdust, price, contentsIv100, contentsLegend
+        })
+      })
+      .then(() => res.redirect('/admin/accounts'))
       .catch(err => next(err))
   }
 }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/accounts', adminController.getAccounts)
+router.put('/accounts/:id', adminController.putAccount)
 
 module.exports = router
