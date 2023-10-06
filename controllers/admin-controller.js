d6d2207817e9b83146017ef837edbd5365c1a1cf
DickLan <pokemaster556610@gmail.com>
2023-10-06 11:05:26 +0800

    add admin delete account feature

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -18,16 +18,15 @@ const adminController = {
     // account在db的id (PK)
     const dbActId = req.params.id
     // load from modal form, and modal form load from previous getAccounts
-    const { accountId, level, startdust, price, contentsIv100, contentsLegend } = req.body
-    console.log('===================')
-    console.log('req.body=', req.body)
-    if (!accountId) throw new Error('accountId is required!')
+    const { accountId, team, level, startdust, price, contentsIv100, contentsLegend, youtube } = req.body
+    // 檢查項目 星砂移除 因為有時沒記錄到
+    if (!accountId || !level || !price || !contentsIv100 || !contentsLegend) throw new Error('All fields is required! (except U2)')
     Account.findByPk(dbActId)
       .then(acct => {
         if (!acct) throw new Error('no this acct admin')
 
         return acct.update({
-          accountId, level, startdust, price, contentsIv100, contentsLegend, youtube
+          accountId, team, level, startdust, price, contentsIv100, contentsLegend, youtube
         })
       })
       .then(() => {
@@ -38,14 +37,15 @@ const adminController = {
   },
   postAccount: (req, res, next) => {
 
-    const { accountId, level, startdust, price, contentsIv100, youtube } = req.body
+    const { accountId, team, level, startdust, price, contentsIv100, youtube } = req.body
     let contentsLegend = req.body.contentsLegend || ('Null／無')
-    console.log('============', accountId, level, price, contentsIv100)
+    // console.log('============', accountId, level, price, contentsIv100)
 
     // 檢查項目 星砂移除 因為有時沒記錄到
     if (!accountId || !level || !price || !contentsIv100 || !contentsLegend) throw new Error('All fields is required! (except U2)')
     Account.create({
       accountId,
+      team,
       level: parseInt(level),
       startdust: parseInt(startdust),
       price: parseInt(price),
@@ -55,6 +55,21 @@ const adminController = {
     })
       .then(() => res.redirect('/admin/accounts'))
       .catch(err => next(err))
+  },
+  deleteAccount: (req, res, next) => {
+    const id = req.params.id
+    if (!id) throw new Error('No this id')
+    Account.findByPk(id)
+      .then(acct => {
+        if (!acct) throw new Error('No this account')
+        acct.destroy()
+      })
+      .then(() => {
+        req.flash('success_msg', 'success delet')
+        res.redirect('/admin/accounts')
+      })
+      .catch(err => next(err))
+
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.put('/accounts/:id', adminController.putAccount)
+router.delete('/accounts/:id', adminController.deleteAccount)
 router.get('/accounts', adminController.getAccounts)
 router.post('/accounts',adminController.postAccount)
 
