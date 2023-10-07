e44751563a740d56b451c1f6737cb59fbf93a241
DickLan <pokemaster556610@gmail.com>
2023-10-08 01:46:56 +0800

    add search function: check accountId/team/level. The rest will be next commit

---
+++ tt.js
@@ -0,0 +1,14 @@
+// const a = ['red', 'blue', 'yellow', 'yet']
+const a = ['abc']
+// console.log(a.includes('red'))
+a.map(b => {
+  console.log(b)
+})
+let b = 'ac'
+// b = [b]
+console.log(typeof (b))
+
+if (typeof (b) === 'string') {
+  console.log('L=1')
+
+}
