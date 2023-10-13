bba74b67ad1e622f525888c8e300d7c1d8e4b3f4
DickLan <pokemaster556610@gmail.com>
2023-10-13 15:06:52 +0800

    add accounts page function Filter and Sort

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,7 +4,8 @@ const adminController = require('../../controllers/admin-controller')
 
 router.put('/accounts/:id', adminController.putAccount)
 router.delete('/accounts/:id', adminController.deleteAccount)
+// router.get('/accounts/sortRule', adminController.getAccounts)
 router.get('/accounts', adminController.getAccounts)
-router.post('/accounts',adminController.postAccount)
+router.post('/accounts', adminController.postAccount)
 
 module.exports = router
