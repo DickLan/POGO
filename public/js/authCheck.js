ed2e165b852b0af94fcbfb0f8641fa62ae725c43
DickLan <pokemaster556610@gmail.com>
2023-11-23 19:55:08 +0800

    add login check on chatbox

---
+++ public/js/authCheck.js
@@ -0,0 +1,16 @@
+const userLoggedIn = user ? true : false
+console.log('userLoggedIn', userLoggedIn)
+const loginCheck = document.querySelector('#log-check-chatbox')
+
+if (userLoggedIn) {
+  // loginCheck.style.display = 'none'
+  loginCheck.classList.remove('d-flex')
+} else {
+  // loginCheck.style.display = 'block'
+  loginCheck.classList.add('d-flex')
+}
+
+window.onload = function () {
+  const userLoggedIn = req.user.isAuthenticaed()
+  console.log('userLoggedIn', userLoggedIn)
+}
